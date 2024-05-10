4c919f8ba7044191cc7c01a9469864c8c2e5d31a
NatashaSharif555 <natashasharif7272@gmail.com>
2024-05-10 14:58:46 +0500

    TASK-2-COLORS

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ export default function App() {
       <SalesPersonDetails/>
       </View>
     
-    
   );
 }
 
---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+
+export const Colors = {
+    brightCyan: '#40E0D0',
+    lightGreen: '#77E9BF',
+    primary200: '#77cff8',
+  
+  };
