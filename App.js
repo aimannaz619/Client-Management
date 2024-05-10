d8c3df3b22906148b1146ec028b3fc38c69def25
aimannaz619 <nazaiman619@gmail.com>
2024-05-10 16:57:18 +0500

    dependencies issue resolved

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+
 import { StyleSheet, Text, View } from "react-native";
 import AllClients from "./screens/clients/AllClients";
 import ClientDetails from "./screens/clients/ClientDetails";
@@ -6,8 +6,13 @@ import AllSalesPersons from "./screens/salesPersons/AllSalesPersons";
 import SalesPersonDetails from "./screens/salesPersons/SalesPersonDetails";
 import { Provider } from "react-redux";
 import store from "./store";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+
 
 export default function App() {
+  const Drawer = createDrawerNavigator()
+ 
   return (
     <Provider store={store}>
       <View style={styles.container}>
