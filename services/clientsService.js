f5ca4de072ad8aa98be61624af795e0439e5d7e3
aimannaz619 <nazaiman619@gmail.com>
2024-05-15 13:33:41 +0500

    TASK_5 connected backend with frontend

--- services/clientsService.js
+++ services/clientsService.js
@@ -1,13 +1,17 @@
+import axios from "axios";
+import { API_URL_GET_ALL_CLIENTS } from "../config/endPoints/client";
 
 // clientsService.js
 export const fetchClients = async () => {
-
   try {
-    const response = require("../util/ClientsData.json");
-   
-    
-    return response;
-  } catch (error) {
-    throw error;
+
+    console.log("why")
+    const response = await axios.get(API_URL_GET_ALL_CLIENTS);
+
+    return response.data;
   }
+  catch (error) {
+    console.log(error, "error")
+  }
+  
 };
